28dd1e6615b05c572f96c5dc6e0e3b5063e912ac
HalforcNull <yaorunan@gmail.com>
2017-10-09 23:43:02 -0500

    move config file to ignore list

--- ScriptRunner/Config.py
+++ ScriptRunner/Config.py
@@ -1,6 +1,3 @@
 class Config(object):
-    SCRIPT_PATH = "C:\DemoScriptFolder\"
-    R_EXE_PATH = "C:\Program Files\r\R-3.4.1\bin\R.exe"
-
-
-    
+    SCRIPT_PATH = "C:/DemoScriptFolder/"
+    R_EXE_PATH = "C:/Program Files/r/R-3.4.1/bin/R.exe"
